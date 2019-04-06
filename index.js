ffa39f86da6eccfce115d1d9f747212a718904ad
huascarm <huascarm@gmail.com>
2019-04-06 02:47:34 -0400

    limite de respuestas

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const restService = express();
+var c = 0;
 
 restService.use(
   bodyParser.urlencoded({
@@ -14,27 +15,55 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
+  c++;
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.echoText
-      ? "Envie esto al humano \""+req.body.queryResult.parameters.echoText + " \"  El dijo esto: \"Hola soy humano\""
+      ? "Respuesta "+c+": Envie esto al humano \""+req.body.queryResult.parameters.echoText + " \"  El dijo esto: \"Hola soy humano\""
       : "Seems like some problem. Speak again."+req.body;
-  
-  return res.json({
 
-  "fulfillmentText": speech,
-  "fulfillmentMessages": [
-    {
-      "text": {
-        "text": [speech]
+  if(c>2){
+    return res.json({
+
+      "fulfillmentText": speech,
+      "fulfillmentMessages": [
+        {
+          "text": {
+            "text": [speech]
+          }
+        }
+      ],
+      "source": "<webhookpn1>",
+    
+      "followupEventInput": {
+        "name": "Welcome",
+        "parameters": {
+          "parameter-name-1": "parameter-value-1",
+          "parameter-name-2": "parameter-value-2"
+        }
       }
-    }
-  ],
-  "source": "<webhookpn1>"
+    
+      });
 
+  }else{
+    return res.json({
 
-  });
+      "fulfillmentText": speech,
+      "fulfillmentMessages": [
+        {
+          "text": {
+            "text": [speech]
+          }
+        }
+      ],
+      "source": "<webhookpn1>"
+    
+    
+      });
+  }
+  
+  
 });
 
 
