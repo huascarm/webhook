e506bfc3ee18297c4b110b6f644f57b953e65c7c
huascarm <huascarm@gmail.com>
2019-04-08 12:23:55 -0400

    implementado chat con el dialog flow

--- index.js
+++ index.js
@@ -2,8 +2,95 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
 
-const restService = express();
+//ejs
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+//socket
+let server = app.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+const io = require("socket.io")(server);
+
+//chat
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", socket => {
+  console.log("Usuario Conectado");
+  socket.username = "Anonimo";
+
+  socket.on("change_username", data => {
+    console.log("Username cambiado");
+    socket.username = data.username;
+  });
+
+  socket.on("message_client", data => {
+    console.log("Mensaje recibido", data);
+    io.sockets.emit("message_server", {
+      username: socket.username,
+      message: data.message
+    });
+  });
+});
+
+restService.post("/echo", function(req, res) {
+  var speech = "";
+  var socket = io.connect("https://habla2.herokuapp.com/");
+
+  if (
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+  ) {
+    socket.emit("message_client", {
+      message: req.body.queryResult.parameters.echoText,
+      username: "bot"
+    });
+  }
+
+  socket.on('message_server', function(data){
+    console.warn('Mensaje recibido SERV CLIENT: ', data);
+    res.json({
+      fulfillmentText: speech,
+      fulfillmentMessages: [
+        {
+          text: {
+            text: data.username+': '+data.message,
+          }
+        }
+      ],
+      source: "<webhookpn1>",
+      outputContexts: [
+        {
+          name:
+            "projects/huascar1/agent/sessions/" +
+            req.body.sessionId +
+            "/contexts/humano",
+          lifespanCount: 5,
+          parameters: {
+            param: "param value"
+          }
+        }
+      ]
+    });
+  })
+
+  //respuesta
+
+});
+
+/*
 var c = 0;
 
 restService.use(
@@ -94,4 +181,4 @@ restService.listen(process.env.PORT || 8000, function() {
 
 restService.get("/propio", function(req, res) {
   res.send('Huascar Answer')
-})
+})*/
---
+++ public/chat.js
@@ -0,0 +1,23 @@
+$(function(){
+    var socket = io.connect('https://habla2.herokuapp.com/');
+    var message = $('#message');
+    var user = $('#user');
+    var send_message = $('#send_message');
+    var send_username = $('#send_username');
+    var chatroom  = $('#chatroom');
+
+    //events
+    send_username.click(function(){
+        console.log('Username', user.val())
+        socket.emit('change_username', {username: user.val()})
+    })
+
+    send_message.click(function(){
+        socket.emit('message_client', {message: message.val(), username: user.val()})
+    })
+
+    socket.on('message_server', function(data){
+        console.warn('Mensaje recibido: ',data)
+        chatroom.append('<li class="list-group-item">'+data.username+': '+data.message+'</li>')
+    })
+})
