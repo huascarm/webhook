36369c108fc307fb0532e99c078732a9302953fa
huascarm <huascarm@gmail.com>
2019-04-08 20:14:06 -0400

    corrijo

--- index.js
+++ index.js
@@ -70,8 +70,16 @@ app.post("/echo", function(req, res) {
     fulfillmentText: speech,
     fulfillmentMessages: [
       {
-        "text": {
-          "text": speech,//serv[serv.length-1].username+': '+serv[serv.length-1].message,
+        card: {
+          "title": "card title",
+          "subtitle": "card text",
+          "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+          "buttons": [
+            {
+              "text": "button text",
+              "postback": "https://assistant.google.com/"
+            }
+          ]
         }
       }
     ],
